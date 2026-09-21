7b1b7302f225382fae4a41f227b20768a631b0eb
Yatnesh <yatnesh65@gmail.com>
2026-09-21 11:55:28 +0530

    get method

--- index.js
+++ index.js
@@ -86,7 +86,23 @@ app.delete('/assignments/:id', async(req, res) => {
         });
     }
 });
-
+app.get('/assignments', async (req, res) => {
+    try{
+        const { submitted } = req.params;
+        let result = await pool.query(
+            `SELECT * FROM assignments
+                WHERE submitted = $1
+                    ORDER BY id DESC`,
+                    [submitted]
+        );
+        res.status(200).json(result.rows);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({
+            errorMessage: 'Server is closed'
+        });
+    }
+});
 
 
 
