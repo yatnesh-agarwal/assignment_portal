0d828dc5346b0671d852f9bf68cfa97ab00748f8
Yatnesh <yatnesh65@gmail.com>
2026-09-21 11:55:00 +0530

    path method

--- index.js
+++ index.js
@@ -40,6 +40,54 @@ app.get('/assignments', async (req, res) => {
     }
 });
 
+app.patch('/assignments/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+        const result = await pool.query(
+            `UPDATE assignments
+            SET submitted = true
+            WHERE id = $1
+            RETURNING *;`,
+            [id]
+        );
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message: 'Assignments Not Found'
+            });
+        };
+        res.status(200).json(result.rows[0]);
+    }catch(err){
+    console.log(err.message);
+    res.status(502).json({
+        errorMessage: 'Server is closed'
+    });
+    }
+});
+
+app.delete('/assignments/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+        let result = await pool.query(
+            `DELETE FROM assignments
+                WHERE id = $1
+                 RETURNING *;`,
+            [id]
+        );
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                errorMessage: 'Assignments Not Found'
+            });
+        }
+        res.status(200).json(result.rows[0])
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({
+            errorMessage: 'Server is closed'
+        });
+    }
+});
+
+
 
 
 app.listen(PORT, () => {
